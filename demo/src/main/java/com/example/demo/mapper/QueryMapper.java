6f0d1d33fd74635c009d1aca491000efd3360d09
ccc-hhh <55867067+ccc-hhh@users.noreply.github.com>
2020-12-08 19:58:29 +0800

    Update QueryMapper.java

--- demo/src/main/java/com/example/demo/mapper/QueryMapper.java
+++ demo/src/main/java/com/example/demo/mapper/QueryMapper.java
@@ -14,12 +14,7 @@ import java.util.List;
 @Repository
 public interface QueryMapper {
 
-    @Select("select RI.room_id,RT.room_name,H.hotel_id,H.hotel_name,avg(RI.price) as avgPrice " +
-            "from room_info RI,room_type RT,hotel H " +
-            "where H.hotel_id=RT.hotel_id and RI.room_id=RT.room_id and (RI.room_id,RT.room_id) in " +
-            "(select room_id,room_id from room_info group by room_id " +
-            "having min(date)<=#{startDate} and max(date)>=#{endDate} and count(date)>#{endDate}-#{startDate} and min(remain)>=#{num}) " +
-            "group by RI.room_id;")
-    List<Query> query(Date startDate, Date endDate, int num);
+    @Select("select;")
+    List<Query> query();
 
 }
