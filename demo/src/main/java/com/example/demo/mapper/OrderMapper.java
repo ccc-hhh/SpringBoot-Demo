03bc62f4d766f0e623552bfbe219cfd8f2778e58
ccc-hhh <55867067+ccc-hhh@users.noreply.github.com>
2020-12-08 19:57:56 +0800

    Update OrderMapper.java

--- demo/src/main/java/com/example/demo/mapper/OrderMapper.java
+++ demo/src/main/java/com/example/demo/mapper/OrderMapper.java
@@ -14,17 +14,13 @@ import java.util.List;
 @Repository
 public interface OrderMapper {
 
-    @Insert("insert into `order` set " +
-            "order_id=0,room_id=#{roomId},start_date=#{startDate},leave_date=#{endDate}," +
-            "amount=#{amount},payment=#{payment},create_date=#{createDate},customer_id=#{userId};")
-    boolean orderInsert(Integer roomId, Date startDate, Date endDate, int amount, float payment, Date createDate, Integer userId);
+    @Insert("insert;")
+    boolean orderInsert();
 
-    @Update("update room_info set remain=remain-#{amount} where room_id=#{roomId} and date>=#{startDate} and date<=#{endDate};")
-    boolean orderUpdate(Integer roomId, Date startDate, Date endDate, int amount);
+    @Update("")
+    boolean orderUpdate();
 
-    @Select("select order_id,room_id,start_date,leave_date,amount,payment,create_date,customer_id as user_id" +
-            " from `order` where room_id=#{roomId} and start_date=#{startDate} and leave_date=#{endDate} and " +
-            "amount=#{amount} and payment=#{payment} and customer_id=#{userId};")
-    Order orderSelect(Integer roomId, Date startDate, Date endDate, int amount, float payment, Integer userId);
+    @Select("")
+    Order orderSelect();
 
 }
